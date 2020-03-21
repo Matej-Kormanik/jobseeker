664fdf265aa20f3bf033c47d83b0e063c08df352
Matej Kormanik <matej.kormanik@gmail.com>
2020-03-21 20:13:48 +0100

    Initial commit

---
+++ src/main/java/com/jobseeker/JobseekerApplication.java
@@ -0,0 +1,13 @@
+package com.jobseeker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobseekerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobseekerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jobseeker/JobseekerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobseeker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobseekerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
