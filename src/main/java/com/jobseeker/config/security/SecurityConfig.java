6360ae4d9f79648dee4f45ba19d4d382e34a841c
Matej Kormanik <matej.kormanik@gmail.com>
2020-05-04 10:44:21 +0200

    [FEAT MK] Added Spring security starter

---
+++ src/main/java/com/jobseeker/config/security/SecurityConfig.java
@@ -0,0 +1,7 @@
+package com.jobseeker.config.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityConfig {
+}
