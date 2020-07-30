a2c634cda25760bacafcc005e2466afe98dec968
Matej Kormanik <matej.kormanik@gmail.com>
2020-07-30 19:42:33 +0200

    added profiles docker, localhost. Works both.

--- src/main/java/com/jobseeker/config/security/SecurityConfig.java
+++ src/main/java/com/jobseeker/config/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.jobseeker.config.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Profile(value = "localhost")
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
